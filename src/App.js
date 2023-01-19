0af7db9e716b3146f7ebeab415b30c9bbb7deac0
Abhishek-Biswas-Deep <109815149+Abhishek-Biswas-Deep@users.noreply.github.com>
2023-01-19 18:04:43 -0400

    bookingapp

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import src="index.html";
+
+function App() {
+  // var x = document.getElementById("Select");
+  // var option = document.createElement("option");
+
+  return (
+    
+  <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          Hello!!!!!
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
